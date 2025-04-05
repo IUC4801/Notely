240b0ef7d993352ed82a0251e8ddaf037cf7bb27
Ayushi Chaudhuri <61285140+IUC4801@users.noreply.github.com>
2025-04-05 19:41:02 +0530

    Added jotai state management

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuFilePlus } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreation = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreation} {...props}>
       <LuFilePlus className="w-4 h-4 text-zinc-100" />
     </ActionButton>
   )
