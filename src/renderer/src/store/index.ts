61e7d3ee6cac3b16382e3e5b0ad1e87474a0cfb7
Ayushi Chaudhuri <61285140+IUC4801@users.noreply.github.com>
2025-04-07 00:36:55 +0530

    Added file creation and deletion functionality

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -58,10 +58,13 @@ export const saveNoteAtom = atom(null, async (get, set, newContent: NoteContent)
   )
 })
 
-export const createEmptyNoteAtom = atom(null, (get, set) => {
+export const createEmptyNoteAtom = atom(null, async (get, set) => {
   const notes = get(notesAtom)
   if (!notes) return
-  const title = `Note ${notes.length + 1}`
+  const title = await window.context.createNote()
+
+  if (!title) return
+
   const newNote: NoteInfo = {
     title,
     lastEditTime: Date.now()
@@ -71,14 +74,21 @@ export const createEmptyNoteAtom = atom(null, (get, set) => {
   set(selectedNoteIndexAtom, 0)
 })
 
-export const deleteNoteAtom = atom(null, (get, set) => {
+export const deleteNoteAtom = atom(null, async (get, set) => {
   const notes = get(notesAtom)
   const selectedNote = get(selectedNoteAtom)
-  if (selectedNote == null || !notes) return
+  if (!selectedNote || !notes) return
+
+  const isDeleted = await window.context.deleteNote(selectedNote.title)
+
+  if (!isDeleted) return
+
+  //filtering out the deleted note
   set(
     notesAtom,
     notes.filter((note) => note.title !== selectedNote.title)
   )
 
+  //deselecting the deleted note
   set(selectedNoteIndexAtom, null)
 })
